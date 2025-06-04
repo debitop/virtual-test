f726dc6d611a2810cd5f55c7a8506b3f26ca7b42
Aleksandr Kochkurkin <89263693878@mail.ru>
2025-06-04 20:34:25 +0300

    add gitignore

--- src/test/java/org/my/app/MyFirstClassTest.java
+++ src/test/java/org/my/app/MyFirstClassTest.java
@@ -1,7 +1,13 @@
 package org.my.app;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyFirstClassTest {
 
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
 }
